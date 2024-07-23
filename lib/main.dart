502f8c92840c473ee5e6b1454e2afe1664ef7c06
jmarciosilva <jmarciosilva@gmail.com>
2024-07-23 20:48:04 -0300

    Utilizando Column, Stack, Row e Container - Aprendendo Alinhamento

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,60 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Row(
+      home: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.red, width: 100, height: 100,
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              ),
+            ],
           ),
-          Container(
-            color: Colors.blue,
-            width: 50,
-            height: 50,
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.red,
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.cyan,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.pink,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.purple,
+                height: 50,
+                width: 50,
+              ),
+            ],
           ),
         ],
       ),
