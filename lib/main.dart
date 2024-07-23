036c53253d4f16ce06441eef5a313874d39291df
jmarciosilva <jmarciosilva@gmail.com>
2024-07-23 20:58:34 -0300

    Adicionado botão, texto, alterado a cor do container

--- lib/main.dart
+++ lib/main.dart
@@ -16,62 +16,84 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pink,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 40,
+              width: 300,
+              child: Text(
+                'Diamante Amarelo',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
               ),
-              Container(
-                color: Colors.blue,
-                width: 50,
-                height: 50,
-              ),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.blue,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.cyan,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.pink,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.purple,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-        ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Apertou o Botao');
+              },
+              child: Text('Aperte o Botão'),
+            )
+          ],
+        ),
       ),
     );
   }
